4afefdd4f6bc6fe010caf78996d1d46fcad34c6b
Maxim <volkonsky13@yandex.ru>
2025-10-04 14:59:58 +0200

    Add front

--- src/front.py
+++ src/front.py
@@ -4,11 +4,12 @@ from src.router import get_user_info
 from fastapi.responses import HTMLResponse
 
 
-router = APIRouter(tags="Front", prefix="/front")
+router = APIRouter(prefix="/front")
 templates = Jinja2Templates(directory="templates")
 
 
-@router.get('{telegram_id}')
-async def get_user_info_front(request: Request, user=Depends(get_user_info)):
+@router.get("/{telegram_id}/", response_class=HTMLResponse)
+async def get_user_info_front(telegram_id: int,request: Request):
+    user = await get_user_info(telegram_id)
     user.update({"request": request})
     return templates.TemplateResponse("index1.html", user)
